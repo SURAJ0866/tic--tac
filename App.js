3b2889aae4acbb411d8978246a438f6be283c78d
SURAJ0866 <106803883+SURAJ0866@users.noreply.github.com>
2023-05-13 10:38:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+//import Namecode from "./component/Namecode";
+//import Validation from "./component/Validation";
+//import Luckynumber from "./component/Luckynumber";
+import Login from "./component/Login";
+
+ function App() {
+  
+  
+
+return(
+  <>
+  
+<Login/>
+</>
+
+)
+
+}
+
+
+
+
+
+
+export default App;
